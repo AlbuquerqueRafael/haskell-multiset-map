5e49d297327ee422b77c2802957de91944206343
klynger <rafael.dantas@ccc.ufcg.edu.br>
2017-12-16 14:55:18 -0300

    Método search implementado

--- bag.hs
+++ bag.hs
@@ -2,17 +2,21 @@
 module MultisetMap (
     Bag(Bag),
     insert,
-    remove
+    remove,
+    search
     ) where
     import Data.Map (Map)
     import qualified Data.Map as Map
     
     data Bag k = Bag (Map k Integer) deriving(Eq, Show)
 
-    -- insert :: Ord k => k -> Bag k -> Bag k
-    insert elem (Bag b) = Bag (Map.insertWith (+) elem 1 b)
+    insert elem (Bag m) = Bag (Map.insertWith (+) elem 1 m)
 
-    -- remove :: Ord k => k -> Bag m -> Maybe Int
     remove elem (Bag m)
         | (Map.findWithDefault 1 elem m == 1) = (Bag (Map.delete elem m))
         | otherwise = Bag (Map.insertWith (+) elem (-1) m)            
+
+    search elem (Bag m)
+        = case Map.lookup elem m of
+            Nothing -> 0
+            Just n -> n
