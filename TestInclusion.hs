0bcab6e3d57146538b608be2b696d7076c09980a
IonesioJunior <ionesiojr@gmail.com>
2017-12-17 21:07:37 -0300

    Added inclusion tests

---
+++ TestInclusion.hs
@@ -0,0 +1,32 @@
+import MultisetMap
+import Test.HUnit
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+bag1 = (Bag (Map.fromList [('A',3),('B',1)]))
+bag2 = (Bag (Map.fromList [('B',2),('A',1)]))
+
+-- Test if some bag is a subset of this
+testInclusion1 = TestCase (assertEqual "Test inclusion 1" True (inclusion bag1 bag1))
+
+bag3 = Bag (Map.empty)
+-- Test if empty bag is a subset of another
+testInclusion2 = TestCase (assertEqual "Test inclusion 2" False (inclusion bag1 bag3))
+
+-- Test if some big bag can be included on another small bag
+bag4 = Bag (Map.fromList [('A',5), ('B', 6), ('C',7) , ('D',8)])
+testInclusion3 = TestCase (assertEqual "Test inclusion 3" False (inclusion bag4 bag1))
+
+-- Test using small bag ( with all another bag elements )
+bag5 = Bag (Map.fromList [('A',3),('B',2), ('D',2)])
+testInclusion4 = TestCase (assertEqual "Test inclusion 4" True (inclusion bag5 bag4))
+
+testInclusion5 = TestCase (assertEqual "Test inclusion 5" False (inclusion bag4 bag5))
+
+-- Test using bag with some attributes , but greater counts
+bag6 = Bag (Map.fromList [('A',10),('B',20), ('D',30)])
+testInclusion6 = TestCase (assertEqual "Test inclusion 6" False (inclusion bag4 bag6))
+
+tests = TestList[testInclusion1, testInclusion2, testInclusion3, testInclusion4, testInclusion5, testInclusion6]
+
+run = runTestTT tests
